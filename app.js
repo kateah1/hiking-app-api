6df6bd2da9fe84941d00b6b8f6bee7f985cae3ab
kateah1 <kate.hurd64@gmail.com>
2016-07-08 12:40:04 -0400

    1240

--- app.js
+++ app.js
@@ -2,16 +2,31 @@
 
 $(document).ready(function() {
 
+// pageload	
 	$(".hidden-advanced").hide();
+	$(".results").hide();
 
+// show advanced search page	
 	$("#advanced").click(function() {
 		$(".homepage").hide();
 		$(".hidden-advanced").show();
+		$(".results").hide();
 	})
 
+// show homepage
 	$(".fa-home").click(function() {
 		$(".homepage").show();
 		$(".hidden-advanced").hide();
+		$(".results").hide();
+	})
+
+// show results page on submit
+	$("#homepage-submit").click(function() {
+		$(".results").show();
+	})
+
+	$("#find-hikes").click(function() {
+		$(".results").show();
 	})
 
 
